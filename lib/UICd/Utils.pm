4c745b57a9f385ccd9adcd12b3328a2406fb4c9f
Mitchell Cooper <cooper879@msn.com>
2012-11-10 00:00:30 -0500

    added global variable utility

--- lib/UICd/Utils.pm
+++ lib/UICd/Utils.pm
@@ -2,6 +2,16 @@
 # utils: commonly used utilities and conveniences.
 package UICd:Utils;
 
-
+# GV
+sub gv {
+    # can't use do{given{ ... }}
+    # compatibility with 5.12 XXX
+    given (scalar @_) {
+        when (1) { return $UICd::GV{+shift}                 }
+        when (2) { return $UICd::GV{+shift}{+shift}         }
+        when (3) { return $UICd::GV{+shift}{+shift}{+shift} }
+    }
+    return
+}
 
 1
